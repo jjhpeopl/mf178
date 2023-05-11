6f42a833608dcb0019853f3bce13e21decc13706
jiajianhong <jiajianhong@meituan.com>
2023-05-11 20:56:53 +0800

    验证微信

--- src/main/java/com/tencent/wxcloudrun/controller/MF178Controller.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MF178Controller.java
@@ -4,13 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * 蜜蜂178处理的控制器
  */
-@Controller
+@RestController
 @Slf4j
 public class MF178Controller {
 

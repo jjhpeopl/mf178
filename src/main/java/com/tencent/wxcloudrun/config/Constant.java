33182ad5214448a552ca41d7aa86b13e7ed49706
jiajianhong <jiajianhong@meituan.com>
2023-05-11 20:46:59 +0800

    验证微信

---
+++ src/main/java/com/tencent/wxcloudrun/config/Constant.java
@@ -0,0 +1,11 @@
+package com.tencent.wxcloudrun.config;
+
+/**
+ * @author jiajianhong
+ * @date 2023/5/11
+ */
+public class Constant {
+
+    public static final String wxToken = "jjhwx";
+
+}
---
+++ src/main/java/com/tencent/wxcloudrun/controller/MF178Controller.java
@@ -0,0 +1,34 @@
+package com.tencent.wxcloudrun.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 蜜蜂178处理的控制器
+ */
+@Controller
+@Slf4j
+public class MF178Controller {
+
+  @Autowired
+  private HttpServletRequest request;
+
+  /**
+   * 主页页面
+   * @return API response html
+   */
+  @GetMapping(value = "/mf178/receive")
+  public String index() {
+    log.info("receive wx msg:{}", request.getQueryString());
+    String signature = request.getParameter("signature");
+    String timestamp = request.getParameter("timestamp");
+    String nonce = request.getParameter("nonce");
+
+    return request.getParameter("echostr");
+  }
+
+}
